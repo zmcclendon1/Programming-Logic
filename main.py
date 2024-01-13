c2e367350f82714d0f30840f022bfbef68935833
Zachery McClendon <zacherymcclendon9@gmail.com>
2024-01-13 10:53:57 -0500

    COP-1000 assignment 1-1

---
+++ main.py
@@ -0,0 +1,14 @@
+#PSEUDOCODE 
+'''
+start
+    output "I'm learning how to program."
+
+    output "That's Awsome!"
+end
+
+'''
+
+
+
+print("I'm learning how to program.")
+print("That's Awesome!")
