40f080d7f4d7bc047485f8fb5ab30dd5efc54199
zmcclendon1 <122585173+zmcclendon1@users.noreply.github.com>
2024-02-12 20:10:07 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-#PSEUDOCODE 
-'''
-start
-    output "I'm learning how to program."
+"""
+Function: This program determines if a student will be admitted or rejected.
+Input:  Interactive
+Output: Accept or Reject
+"""
 
-    output "That's Awsome!"
-end
+# Get input and convert to correct data type for testScore and classRank
+testScore = input("")
+classRank = input("")
 
-'''
-
-
-
-print("I'm learning how to program.")
-print("That's Awesome!")
+testScore = int(testScore)
+classRank = int(classRank)
+# Test using admission requirements and print Accept or Reject
+if testScore >= 90:
+    if classRank >= 25:
+        print("Accept")
+    else:
+        print("Reject")
+else:
+    if testScore >= 80:
+        if classRank >= 50:
+            print("Accept")
+        else:
+            print("Reject")
+    else:
+        if testScore >= 70:
+            if classRank >= 75:
+                print("Accept")
+            else:
+                print("Reject")
+        else:
+            print("Reject")
