a6eea120c402fe6ce6aba0650661674c73e96893
zmcclendon1 <122585173+zmcclendon1@users.noreply.github.com>
2024-04-27 16:10:47 -0400

    Add files via upload

--- CarFinder.py
+++ CarFinder.py
@@ -1,45 +1,62 @@
 # CONSTANTS
-allowedVehicleList = [ 'Ford F-150', 'Chevrolet Silverado', 'Tesla CyberTruck', 'Toyota Tundra', 'Nissan Titan', 'Rivian R1T', 'Ram 1500' ]
+allowedVehicleListFilePath = "AllowedVehicleList.txt"
 
-while True:
-    print("****************\n AUTOCARFINDER \n *******************")
-    print("\n1. PRINT LIST OF AVAILABLE CARS\nor\n")
-    print("2. SEARCH FOR VEHICLE\n")
-    print("3. ADD AUTHORIZED VEHICLE\n")
-    print("4. DELETE AUTHORIZED VEHICLE\n")
-    print("5. EXIT")
+# Function to read the allowed vehicle list from the file
+def read_vehicle_list():
+    with open(allowedVehicleListFilePath, "r") as file:
+        return file.read().splitlines()
 
-    menuInput = input("Enter 1, 2, 3, 4, or 5: ")
+# Function to write the updated vehicle list back to the file
+def write_vehicle_list(vehicle_list):
+    with open(allowedVehicleListFilePath, "w") as file:
+        for vehicle in vehicle_list:
+            file.write(vehicle + "\n")
 
-    if int(menuInput) == 5:
-        print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
-        break
+# Main application loop
+def main():
+    vehicle_list = read_vehicle_list()
 
-    if int(menuInput) == 1:
-        print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
-        for element in allowedVehicleList:
-            print(element)
-    if int(menuInput) == 2:
-        vehicleInput = input("Enter Vehicle: ")
-        isValid = False 
-        for vehicle in allowedVehicleList:
-            if vehicleInput == vehicle:
-                print(f"{vehicleInput} is a valid vehicle")
-                isValid = True 
-                break 
-        if not isValid: 
-            print(f"{vehicleInput} is not an authorized vehicle, if you received this in error please check the spelling and try again")
-    if int(menuInput) == 3:
-        newVehicle = input("Please Enter the full Vehicle name you would like to add: ")
-        allowedVehicleList.append(newVehicle)
-        print(f"You have added {newVehicle} as an authorized vehicle")
-      
+    while True:
+        print("****************\n AUTOCARFINDER \n *******************")
+        print("\n1. PRINT LIST OF AVAILABLE CARS\nor\n")
+        print("2. SEARCH FOR VEHICLE\n")
+        print("3. ADD AUTHORIZED VEHICLE\n")
+        print("4. DELETE AUTHORIZED VEHICLE\n")
+        print("5. EXIT")
 
-    if int(menuInput) == 4:
-        deletedVehicle = input("Please Enter the full Vehicle name you would like to REMOVE: ")
-        bool = input(f"Are you sure you want to remove {deletedVehicle} from the Authorized Vehicles List?")
-        if bool == "yes":
-            allowedVehicleList.remove(deletedVehicle)
-            print(f"You have REMOVED {deletedVehicle} as an authorized vehicle")
-        elif bool == "no":
-            print("Thank you")
+        menu_input = input("Enter 1, 2, 3, 4, or 5: ")
+
+        if int(menu_input) == 5:
+            print("Thank you for using the AutoCountry Vehicle Finder, good-bye!")
+            break
+
+        if int(menu_input) == 1:
+            print("The AutoCountry sales manager has authorized the purchase and selling of the following vehicles:")
+            for vehicle in vehicle_list:
+                print(vehicle)
+
+        if int(menu_input) == 2:
+            vehicle_input = input("Enter Vehicle: ")
+            if vehicle_input in vehicle_list:
+                print(f"{vehicle_input} is a valid vehicle")
+            else:
+                print(f"{vehicle_input} is not an authorized vehicle, if you received this in error please check the spelling and try again")
+
+        if int(menu_input) == 3:
+            new_vehicle = input("Please Enter the full Vehicle name you would like to add: ")
+            vehicle_list.append(new_vehicle)
+            write_vehicle_list(vehicle_list)
+            print(f"You have added {new_vehicle} as an authorized vehicle")
+
+        if int(menu_input) == 4:
+            deleted_vehicle = input("Please Enter the full Vehicle name you would like to REMOVE: ")
+            confirmation = input(f"Are you sure you want to remove {deleted_vehicle} from the Authorized Vehicles List? (yes/no): ")
+            if confirmation.lower() == "yes":
+                vehicle_list.remove(deleted_vehicle)
+                write_vehicle_list(vehicle_list)
+                print(f"You have REMOVED {deleted_vehicle} as an authorized vehicle")
+            elif confirmation.lower() == "no":
+                print("Thank you")
+
+if __name__ == "__main__":
+    main()
